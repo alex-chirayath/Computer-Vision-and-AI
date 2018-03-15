ed891f8b6f0e803e0a5f45da79d0da305de86638
Alex Chirayath <22260017+mscsalex@users.noreply.github.com>
2018-03-15 18:29:07 -0400

    MLP and basic CNN model skeleton

---
+++ DigitRecog/models_digit_recog.py
@@ -0,0 +1,34 @@
+from data_digit_recog import *
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras import backend as K
+K.set_image_dim_ordering('th')
+
+
+def mlp_model(num_pixels,num_classes):
+	# create model
+	model = Sequential()
+	model.add(Dense(num_pixels, input_dim=num_pixels, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(num_classes, kernel_initializer='normal', activation='softmax'))
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+
+
+
+def cnn_model(num_classes):
+	model = Sequential()
+	model.add(Conv2D(32, (5, 5), input_shape=(1, 28, 28), activation='relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Dropout(0.2))
+	model.add(Flatten())
+	model.add(Dense(128, activation='relu'))
+	model.add(Dense(num_classes, activation='softmax'))
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+
+
+
+	
